cc76a6b568511ce061fdc8774bc62e8d79d14335
matheusschuetz <matheusschuetz0213@gmail.com>
2020-02-17 09:11:38 -0300

    model

---
+++ Model/Evento.py
@@ -0,0 +1,7 @@
+class Evento:
+    def __init__(self, ID, NOME, DATA, DESCRICAO, LOCAL):
+        self.ID = ID
+        self.NOME = NOME
+        self.DATA = DATA
+        self.DESCRICAO = DESCRICAO
+        self.LOCAL = LOCAL
