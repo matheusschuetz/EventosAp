52da313bab34090c66f9aaaeee126e29facca7c8
ruanmarcoss <silva.ruanmarcos@gmail.com>
2020-02-17 10:19:21 -0300

    Dao

--- Dao/Evento_dao.py
+++ Dao/Evento_dao.py
@@ -1,7 +1,14 @@
+from Model.Evento import Evento
+class EventoDao()
+    def __init__(self):
+        self.model = Evento
 
-def list_all()
-    lista = []
-    arquivo = open(r"C:\Users\900152\Documents\EventosApi\Tabela", "r")
-    for i in arquivo:
-        lista.append(i)
-    print(lista)
+    def list_all()
+        lista = []
+        arquivo = open(r"C:\Users\900152\Documents\EventosApi\Tabela", "r")
+        for i in arquivo:
+            lista.append(i)
+        return lista
+
+a = EventoDao()
+print(a.list_all())
