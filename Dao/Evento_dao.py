38ad7ed102d933cc78b8564ac92e72245e015146
ruanmarcoss <silva.ruanmarcos@gmail.com>
2020-02-17 11:49:03 -0300

    View

--- Dao/Evento_dao.py
+++ Dao/Evento_dao.py
@@ -1,25 +1,21 @@
 from Model.Evento import Evento
 class EventoDao():
     def __init__(self):
-        self.model = Evento
+        arquivo = open(r'C:\Users\900160\Documents\EventosApi\Tabela', 'r')
+        self.lista = []
+        for i in arquivo:
+            self.lista.append(i.split(';'))
+        arquivo.close()
 
     def list_all(self):
         lista = []
-        arquivo = open(r'C:\Users\900160\Documents\EventosApi\Tabela','r')
-        for i in arquivo:
-            linha = i.split(';')
-            list = {linha[0], linha[1], linha[2], linha[3], linha[4]}
-        arquivo.close()
-        return print(list)
+        for i in self.lista:
+            dic = {'ID':i[0],'NOME':i[1],'DATA':i[2],'DESCRICAO':i[3],'LOCAL':i[4]}
+        lista.append(dic)
+        print(lista)
+        return lista
 
-    # def inserir(self):
-    #     lista_q = []
-    #     arquivo = open(r'C:\Users\900160\Documents\EventosApi\Tabela', 'a')
-    #     for i in arquivo:
-    #         linha = i.split(';')
-    #         list = {linha[0], linha[1], linha[2], linha[3], linha[4]}
-    #         lista_q.append(list)
-    #     return lista_q
+
 
 
 a = EventoDao()
